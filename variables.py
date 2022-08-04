4d705f6cc4cad5eba9861e057d853f2dc544bc81
Marco Aguero <marco.aguero@hotmail.com>
2022-08-03 22:55:06 -0400

    Adding variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*- Line 2
+#----------------------------------------------------------------------------
+# Created By  : mawero83
+# Created Date: 08/03/2022
+# version ='1.0'
+
+""" This program is an example of different Python variables """
+
+# In Python you don't need to specify the datatype, the data itself drives the type
+
+# Integer
+my_integer=10
+my_float=1.1
+my_string="UoPeople"
+my_list=[1,2,3,4,5]
+# The following is a special type
+my_dictionary={}
+
