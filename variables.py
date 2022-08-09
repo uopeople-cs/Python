2025eb6d32bdf961eb153564e71e43245d8b1eec
Marco Aguero <44516809+mawero83@users.noreply.github.com>
2022-08-09 17:11:44 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -16,3 +16,30 @@ my_list=[1,2,3,4,5]
 # The following is a special type
 my_dictionary={}
 
+# A dictionary is a key, value pair, it is also called a hash table and comes with great
+# performance benefits
+my_dictionary['item1']="content 1"
+
+# You can get the item by calling it like this:
+my_dictionary['item1']
+
+# or by using the get method
+my_dictionary.get("item1")
+
+# If the item doesn't exist in the dictionary, the response will be empty
+# so you should consider to use a default
+my_dictionary.get("item1","your default value")
+
+# In this case, if the item doesn't exist, you will get a default value back
+
+# Your dictionary object has iterable objects, like .keys(), or .values(), and .items()
+# that makes it easy to iterate through it, for example.
+
+# .items provides a list of sets = [()] with all the key value pairs.
+
+for k, v in my_dictionary.items():
+    print ("The key is "+k+" and the value is "+v)
+
+# The example above walks a dictionary and shows the keys and values
+# Note the usage of the print method, it is using in-t
+
